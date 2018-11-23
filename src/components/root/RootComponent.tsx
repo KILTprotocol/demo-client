cd1cc1bbc56c0f5ecfc0cf3c5befe11d3729758e
Mario Neises <git@mnio.net>
2018-11-23 14:19:24 +0100

    add demo from prototype sdk

--- src/components/root/RootComponent.tsx
+++ src/components/root/RootComponent.tsx
@@ -1,8 +1,14 @@
+import { Demo } from '@kiltprotocol/prototype-sdk'
 import * as React from 'react'
 
 class RootComponent extends React.Component {
   public render() {
-    return <h1>Welcome to KILT Prototype Client</h1>
+    return (
+      <div>
+        <h1>Welcome to KILT Prototype Client</h1>
+        The SDK says: {new Demo().hello()}
+      </div>
+    )
   }
 }
 
