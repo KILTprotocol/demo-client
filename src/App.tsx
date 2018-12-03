c600b2452b004ec390ce24d01cc307d1bacb44cb
Mario Neises <git@mnio.net>
2018-12-03 16:53:39 +0100

    refactor(component): move header to subfolder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import './App.css'
-import HeaderComponent from './components/HeaderComponent'
+import HeaderComponent from './components/header/HeaderComponent'
 import Routes from './components/Routes'
 
 class App extends React.Component {
--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/logo.png'
+import logo from '../../assets/logo.png'
 
 const HeaderComponent: React.FunctionComponent = props => {
   return (
