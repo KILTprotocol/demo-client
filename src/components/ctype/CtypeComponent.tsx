79432b2114e7f5070aa454f134a10d0b45f4c9c0
Mario Neises <git@mnio.net>
2018-12-12 17:22:29 +0100

    feat(ctype): load list from registry

--- src/components/ctype/CtypeComponent.tsx
+++ src/components/ctype/CtypeComponent.tsx
@@ -2,37 +2,54 @@ import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
 import { Link, withRouter } from 'react-router-dom'
 import If from '../../common/If'
+import ctypeRepository from '../../services/CtypeRepository'
+import { CType } from '../../types/Ctype'
 
 type Props = RouteComponentProps<{
   hash?: string
 }>
 
-class CtypeComponent extends React.Component<Props, {}> {
+type State = {
+  ctypes: CType[]
+}
+
+class CtypeComponent extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props)
+
+    this.state = {
+      ctypes: [],
+    }
+  }
+
+  public componentDidMount() {
+    void this.init()
   }
 
   public render() {
     const hash = this.props.match.params.hash
+
+    const list = this.state.ctypes.map(ctype => (
+      <li key={ctype.key}>
+        <Link to={`/ctype/${ctype.key}`}>{ctype.key}</Link>
+      </li>
+    ))
+
     return (
       <div>
         <h1 className="App-title">Ctype Manager</h1>
         <If
           condition={!!hash}
           then={<div>Current hash: {hash}</div>}
-          else={
-            <ul>
-              <li>
-                <Link to={'/ctype/123'}>CTYPE 123</Link>
-              </li>
-              <li>
-                <Link to={'/ctype/ABC'}>CTYPE ABC</Link>
-              </li>
-            </ul>
-          }
+          else={<ul>{list}</ul>}
         />
       </div>
     )
+  }
+
+  private async init() {
+    const ctypes = await ctypeRepository.findAll()
+    this.setState({ ctypes })
   }
 }
 
--- src/services/CtypeRepository.ts
+++ src/services/CtypeRepository.ts
@@ -1,15 +1,22 @@
+import axios from 'axios'
 import Optional from 'typescript-optional'
 import { CType } from '../types/Ctype'
 
 // TODO: add tests, create interface for this class to be implemented as mock (for other tests)
 
 class CtypeRepository {
+  private static readonly URL = `${process.env.REACT_APP_SERVICE_HOST}:${
+    process.env.REACT_APP_SERVICE_PORT
+  }/ctype`
+
   public async findByKey(key: string): Promise<Optional<CType>> {
     return Promise.reject('implement')
   }
 
-  public async findAll(): Promise<Optional<CType[]>> {
-    return Promise.reject('implement')
+  public async findAll(): Promise<CType[]> {
+    const response = await axios.get(CtypeRepository.URL)
+    const ctypes = response.data as CType[]
+    return ctypes
   }
 
   public async register(cType: CType): Promise<CType> {
