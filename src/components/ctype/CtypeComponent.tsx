9348102c5b1061391f2998ef156caee328280ca7
Timo Welde <tjwelde@gmail.com>
2018-12-13 15:03:18 +0100

    feat: ctype editor and submit to chain

--- src/components/ctype/CtypeComponent.tsx
+++ src/components/ctype/CtypeComponent.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
 import { Link, withRouter } from 'react-router-dom'
+
+import blockchainService from 'src/services/BlockchainService'
+import { ApiPromise } from '@polkadot/api'
+
+import { Keyring } from '@polkadot/keyring'
+import { stringToU8a, u8aToHex } from '@polkadot/util'
+import {
+  keccakAsU8a,
+  naclKeypairFromSeed,
+  naclSign,
+  naclVerify,
+} from '@polkadot/util-crypto'
+
+import CtypeEditorComponent from './CtypeEditorComponent'
+
 import If from '../../common/If'
 
 type Props = RouteComponentProps<{
@@ -8,8 +23,71 @@ type Props = RouteComponentProps<{
 }>
 
 class CtypeComponent extends React.Component<Props, {}> {
+  public state = {
+    connected: false,
+    schema: '{ "title": "My New Schema" }',
+  }
+
+  // @ts-ignore
+  private api: ApiPromise
+
   constructor(props: Props) {
     super(props)
+    this.submit = this.submit.bind(this)
+  }
+
+  public componentDidMount() {
+    this.connect()
+  }
+
+  public async connect() {
+    // TODO: test unmount and host change
+    // TODO: test error handling
+    this.api = await blockchainService.connect()
+    this.setState({ connected: true })
+    // @ts-ignore
+    window.api = this.api
+  }
+
+  public async submit() {
+    // needs hash and signature
+
+    const seedAlice = 'Alice'.padEnd(32, ' ')
+
+    const keyring = new Keyring()
+
+    const Alice = keyring.addFromSeed(stringToU8a(seedAlice))
+    console.log(keyring)
+
+    const { secretKey, publicKey: pubKey } = naclKeypairFromSeed(
+      stringToU8a(seedAlice)
+    )
+
+    const hash = keccakAsU8a(this.state.schema)
+
+    // Both approaches lead to same signature
+    const messageSignature = naclSign(hash, secretKey)
+    console.log(`Message signature: ${u8aToHex(messageSignature)}`)
+    const signature = Alice.sign(hash)
+    console.log(`Signature: ${u8aToHex(signature)}`)
+
+    const isValidSignature = naclVerify(hash, messageSignature, pubKey)
+    console.log(`Was the message correctly signed? ${isValidSignature}`)
+
+    const ctypeAdd = this.api.tx.ctype.add(hash, signature)
+
+    const nonce = await this.api.query.system.accountNonce(Alice.address())
+    if (nonce) {
+      const signed = ctypeAdd.sign(Alice, nonce.toHex())
+      signed
+        .send(status => {
+          console.log(`current status ${status.type}`)
+          console.log(status)
+        })
+        .then(hash => {
+          console.log(`submitted with hash ${hash}`)
+        })
+    }
   }
 
   public render() {
@@ -17,6 +95,17 @@ class CtypeComponent extends React.Component<Props, {}> {
     return (
       <div>
         <h1 className="App-title">Ctype Manager</h1>
+        <CtypeEditorComponent
+          schema={this.state.schema}
+          updateSchema={this.updateSchema}
+        />
+        <br />
+        <button
+          disabled={this.state.connected ? false : true}
+          onClick={this.submit}
+        >
+          Submit
+        </button>
         <If
           condition={!!hash}
           then={<div>Current hash: {hash}</div>}
@@ -33,6 +122,12 @@ class CtypeComponent extends React.Component<Props, {}> {
         />
       </div>
     )
+  }
+
+  private updateSchema = (schema: string) => {
+    this.setState({
+      schema,
+    })
   }
 }
 
---
+++ src/components/ctype/CtypeEditorComponent.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react'
+
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript.js'
+import './codemirror.css'
+import { Controlled as CodeMirror } from 'react-codemirror2'
+
+import Ajv from 'ajv'
+import MetaSchema from 'ajv/lib/refs/json-schema-draft-07.json'
+
+import { js as beautify } from 'js-beautify'
+
+type Props = {
+  schema: string
+  updateSchema: (schema: string) => void
+}
+
+class CtypeEditorComponent extends React.Component<Props, {}> {
+  public state = {
+    errors: [],
+    valid: true,
+  }
+
+  private ajv: any
+
+  constructor(props: Props) {
+    super(props)
+    const schema = MetaSchema as any
+    schema.additionalProperties = false
+    this.ajv = new Ajv({
+      meta: false,
+    })
+    this.ajv.addMetaSchema(schema)
+  }
+
+  public render() {
+    return (
+      <div>
+        <div style={{ textAlign: 'left', maxWidth: '700px', margin: '0 auto' }}>
+          <CodeMirror
+            value={this.props.schema}
+            options={{
+              indentUnit: 4,
+              lineNumbers: true,
+              mode: { name: 'javascript', json: true },
+            }}
+            onBeforeChange={this.changeSchema}
+            onChange={this.validate}
+          />
+        </div>
+        <button onClick={this.autoformat}>Autoformat</button>
+        <div
+          style={{
+            backgroundColor: this.state.valid ? 'green' : 'red',
+            height: '15px',
+            margin: '0 auto',
+            width: '170px',
+          }}
+        />
+        {this.state.errors.map(error => (
+          <div>{error}</div>
+        ))}
+      </div>
+    )
+  }
+
+  private validate = (editor: any, data: any, value: string) => {
+    let validate = ''
+    try {
+      validate = this.ajv.validateSchema(JSON.parse(value))
+    } catch (e) {
+      console.log(e)
+      this.setState({ valid: false, errors: ['JSON invalid'] })
+      return
+    }
+    if (!validate) {
+      const errors = this.ajv.errors.map((error: any) => {
+        return error.message
+      })
+      this.setState({
+        errors,
+        valid: false,
+      })
+      console.log(this.ajv.errors)
+    } else {
+      this.setState({ valid: true, errors: [] })
+    }
+  }
+
+  private changeSchema = (editor: any, data: any, value: string) => {
+    this.props.updateSchema(value)
+  }
+
+  private autoformat = () => {
+    const formatted = beautify(this.props.schema)
+    this.props.updateSchema(formatted)
+  }
+}
+
+export default CtypeEditorComponent
