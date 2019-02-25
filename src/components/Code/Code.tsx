c286352d08b5b8c83bda703762b9d884559df1c8
Michael Woywode <>
2019-02-25 17:01:34 +0100

    fix: restore old <Code> implementation
    since current json view cannot display primitives

--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -12,17 +12,45 @@ type State = {}
 class Code extends React.Component<Props, State> {
   public render() {
     const { children, collapsed } = this.props
-    return (
-      <ReactJson
-        src={children as object}
-        name={false}
-        theme="monokai"
-        collapsed={collapsed != null ? collapsed : 1}
-        collapseStringsAfterLength={30}
-        enableClipboard={false}
-        displayDataTypes={false}
-      />
-    )
+    let output: string | number
+
+    if (children && typeof children === 'object') {
+      return (
+        <ReactJson
+          src={children as object}
+          name={false}
+          theme="monokai"
+          collapsed={collapsed != null ? collapsed : 1}
+          collapseStringsAfterLength={30}
+          enableClipboard={false}
+          displayDataTypes={false}
+        />
+      )
+    } else {
+      switch (typeof children) {
+        case 'undefined':
+          output = 'undefined'
+          break
+        case 'boolean':
+          output = children ? 'true' : 'false'
+          break
+        case 'string':
+          output = children as string
+          break
+        case 'number':
+          output = children as number
+          break
+        case 'symbol':
+          output = 'symbol'
+          break
+        case 'function':
+          output = 'function'
+          break
+        default:
+          output = 'null'
+      }
+      return <pre className="Code">{output}</pre>
+    }
   }
 }
 
--- src/components/CtypeDetailView/CtypeDetailView.tsx
+++ src/components/CtypeDetailView/CtypeDetailView.tsx
@@ -27,7 +27,7 @@ const CtypeDetailView = ({ cType }: Props) => {
             <div>
               <label>Definition</label>
               <div>
-                <Code>cType.cType</Code>
+                <Code>{cType.cType}</Code>
               </div>
             </div>
           </div>
