b4e6d2b33826c55a911a7c066bb940dc26bd3864
Michael Woywode <>
2019-04-10 17:20:57 +0200

    feat: add client & sdk version to utilities component

--- src/components/Utilities/Utilities.tsx
+++ src/components/Utilities/Utilities.tsx
@@ -3,6 +3,9 @@ import * as React from 'react'
 import ChainStats from '../ChainStats/ChainStats'
 import TestUserFeedback from '../TestUserFeedback/TestUserFeedback'
 
+import sdkPackage from '@kiltprotocol/prototype-sdk/package.json'
+import clientPackage from '../../../package.json'
+
 import './Utilities.scss'
 
 type Props = {}
@@ -10,9 +13,21 @@ type State = {}
 
 class Utilities extends React.Component<Props, State> {
   public render() {
+    console.log('clientPackage', clientPackage)
     return (
       <section className="Utilities">
         <h1>Utilities</h1>
+        <section className="versions">
+          <h2>Versions</h2>
+          <div>
+            <label>Client</label>
+            <div>{clientPackage.version}</div>
+          </div>
+          <div>
+            <label>SDK</label>
+            <div>{sdkPackage.version}</div>
+          </div>
+        </section>
         <ChainStats />
         <TestUserFeedback />
       </section>
