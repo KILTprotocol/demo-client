d4b8d867f7707a42b70883b899bab9120e44852e
Michael Woywode <>
2019-04-11 16:21:17 +0200

    feat: add cType caching without persisting
    fixes key in attestationList
    fixes table cell alignment

--- src/services/CtypeRepository.ts
+++ src/services/CtypeRepository.ts
@@ -1,20 +1,48 @@
 import * as sdk from '@kiltprotocol/prototype-sdk'
 
+import * as CTypes from '../state/ducks/CTypes'
+import PersistentStore from '../state/PersistentStore'
 import { ICType } from '../types/Ctype'
+
 import { BasePostParams } from './BaseRepository'
 
 // TODO: add tests, create interface for this class to be implemented as mock
 // (for other tests)
 
 class CTypeRepository {
-  public static async findByHash(hash: sdk.ICType['hash']): Promise<ICType> {
-    return fetch(`${CTypeRepository.URL}/${hash}`).then(response =>
-      response.json()
-    )
+  public static async findByHash(
+    hash: sdk.ICType['hash']
+  ): Promise<void | ICType> {
+    const storedCType = CTypes.getCType(PersistentStore.store.getState(), hash)
+
+    if (storedCType) {
+      return Promise.resolve(storedCType)
+    }
+
+    return fetch(`${CTypeRepository.URL}/${hash}`)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        return response
+      })
+      .then(response => response.json())
+      .then((cType: ICType) => {
+        PersistentStore.store.dispatch(CTypes.Store.addCType(cType))
+        return cType
+      })
+      .catch(() => {
+        console.log(`Could not fetch CType with hash '${hash}'`)
+      })
   }
 
   public static async findAll(): Promise<ICType[]> {
-    return fetch(`${CTypeRepository.URL}`).then(response => response.json())
+    return fetch(`${CTypeRepository.URL}`)
+      .then(response => response.json())
+      .then((cTypes: ICType[]) => {
+        PersistentStore.store.dispatch(CTypes.Store.addCTypes(cTypes))
+        return CTypes.getCTypes(PersistentStore.store.getState())
+      })
   }
 
   public static async register(cType: ICType): Promise<Response> {
@@ -22,10 +50,6 @@ class CTypeRepository {
       ...BasePostParams,
       body: JSON.stringify(cType),
     })
-  }
-
-  public static async removeAll() {
-    return Promise.reject('implement')
   }
 
   private static readonly URL = `${process.env.REACT_APP_SERVICE_HOST}:${
