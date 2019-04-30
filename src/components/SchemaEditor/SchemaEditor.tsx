7deb702aae2cb79024c182a77025f87acb7c148c
Timo Welde <tjwelde@gmail.com>
2019-04-30 14:52:43 +0200

    feat: update json-schema-editor to fix time format issue

--- src/components/SchemaEditor/SchemaEditor.tsx
+++ src/components/SchemaEditor/SchemaEditor.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { JSONEditor } from 'react-schema-based-json-editor'
 import * as common from 'schema-based-json-editor'
-import { Claim } from '@kiltprotocol/prototype-sdk'
 
 import './SchemaEditor.scss'
 
@@ -11,50 +10,17 @@ type Props = {
   updateValue: (value: common.ValueType, _isValid: boolean) => void
 }
 
-class SchemaEditor extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props)
-
-    this.prepareTimeValuesAndUpdate = this.prepareTimeValuesAndUpdate.bind(this)
-  }
-
-  public render() {
-    const { schema, initialValue } = this.props
-    return (
-      <div className="schema-based-json-editor">
-        <JSONEditor
-          schema={schema}
-          initialValue={initialValue}
-          updateValue={this.prepareTimeValuesAndUpdate}
-          icon="fontawesome5"
-        />
-      </div>
-    )
-  }
-
-  private prepareTimeValuesAndUpdate(
-    contents: Claim['contents'],
-    isValid: boolean
-  ) {
-    const { updateValue, schema } = this.props
-
-    for (const prop in contents) {
-      if (
-        contents.hasOwnProperty(prop) &&
-        // is time format in schema
-        // @ts-ignore
-        schema.properties[prop].format === 'time' &&
-        // not empty
-        contents[prop] &&
-        // Only, if format is 00:00 (where 0 can be any digit), add another :00
-        contents[prop].match(/^\d{2}:\d{2}$/)
-      ) {
-        contents[prop] = `${contents[prop]}:00`
-      }
-    }
-
-    updateValue(contents, isValid)
-  }
+const SchemaEditor = ({ schema, initialValue, updateValue }: Props) => {
+  return (
+    <div className="schema-based-json-editor">
+      <JSONEditor
+        schema={schema}
+        initialValue={initialValue}
+        updateValue={updateValue}
+        icon="fontawesome5"
+      />
+    </div>
+  )
 }
 
 export default SchemaEditor
