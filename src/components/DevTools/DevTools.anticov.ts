2287c5cb54a0fa9338f0eddc2d7db2678dba7f77
Raphael Flechtner <flechtner@galaniprojects.de>
2020-06-04 14:13:19 +0200

    chore: requested changes

--- src/components/DevTools/DevTools.anticov.ts
+++ src/components/DevTools/DevTools.anticov.ts
@@ -1,6 +1,11 @@
 import * as sdk from '@kiltprotocol/sdk-js'
 import { IMetadata } from '@kiltprotocol/sdk-js/build/types/CTypeMetadata'
-import ANTICOV_CONFIG from './data/anticov.json'
+import {
+  ROOT_SEED,
+  CTYPE,
+  CTYPE_METADATA,
+  DELEGATION_ROOT_ID,
+} from './data/anticov.json'
 import { IMyIdentity } from '../../types/Contact'
 import CTypeRepository from '../../services/CtypeRepository'
 import { BalanceUtilities } from '../../services/BalanceUtilities'
@@ -11,28 +16,28 @@ import FeedbackService, {
   notifyFailure,
 } from '../../services/FeedbackService'
 
-const root = sdk.Identity.buildFromMnemonic(ANTICOV_CONFIG.ROOT_SEED)
+const root = sdk.Identity.buildFromMnemonic(ROOT_SEED)
 
-const ctype = sdk.CType.fromCType(ANTICOV_CONFIG.CTYPE as sdk.ICType)
+const ctype = sdk.CType.fromCType(CTYPE as sdk.ICType)
 ctype.owner = root.address
-const metadata: IMetadata = ANTICOV_CONFIG.CTYPE_METADATA
+const metadata: IMetadata = CTYPE_METADATA
 
 const delegationRoot = new sdk.DelegationRootNode(
-  ANTICOV_CONFIG.DELEGATION_ROOT_ID,
+  DELEGATION_ROOT_ID,
   ctype.hash,
   root.address
 )
 
-async function newDelegation(delegee: IMyIdentity): Promise<void> {
+async function newDelegation(delegate: IMyIdentity): Promise<void> {
   const delegationNode = new sdk.DelegationNode(
     sdk.UUID.generate(),
     delegationRoot.id,
-    delegee.identity.address,
+    delegate.identity.address,
     [sdk.Permission.ATTEST]
   )
-  const signature = delegee.identity.signStr(delegationNode.generateHash())
+  const signature = delegate.identity.signStr(delegationNode.generateHash())
   await delegationNode.store(root, signature)
-  notifySuccess(`Delegation successfully created for ${delegee.metaData.name}`)
+  notifySuccess(`Delegation successfully created for ${delegate.metaData.name}`)
   await DelegationsService.importDelegation(
     delegationNode.id,
     'AntiCov Attester',
@@ -44,7 +49,7 @@ async function newDelegation(delegee: IMyIdentity): Promise<void> {
     content: { delegationId: delegationNode.id, isPCR: false },
   }
   await MessageRepository.sendToAddresses(
-    [delegee.identity.address],
+    [delegate.identity.address],
     messageBody
   )
 }
@@ -75,19 +80,19 @@ async function verifyOrAddCtypeAndRoot(): Promise<void> {
   }
 }
 
-export async function setupAndDelegate(delegee: IMyIdentity): Promise<void> {
+export async function setupAndDelegate(delegate: IMyIdentity): Promise<void> {
   const blockUi = FeedbackService.addBlockUi({
     headline: 'Creating AntiCov Delegation',
   })
   try {
-    blockUi.updateMessage('transferring funds to AntiCov authority')
+    blockUi.updateMessage('Transferring funds to AntiCov authority')
     await new Promise(resolve => {
-      BalanceUtilities.makeTransfer(delegee, root.address, 4, () => resolve())
+      BalanceUtilities.makeTransfer(delegate, root.address, 4, () => resolve())
     })
-    blockUi.updateMessage('setting up CType and Root Delegation')
+    blockUi.updateMessage('Setting up CType and Root Delegation')
     await verifyOrAddCtypeAndRoot()
-    blockUi.updateMessage('creating Delegation Node for current identity')
-    await newDelegation(delegee)
+    blockUi.updateMessage('Creating Delegation Node for current identity')
+    await newDelegation(delegate)
   } catch (error) {
     notifyFailure(`Failed to set up Delegation Node: ${error}`)
   }
